9669d83afae12e5b3351bf29bf3a21d1b9300393
Divya shrivastava <divyashrivastava@Divyas-MacBook-Air.local>
2020-08-08 14:27:06 +0530

    commitN

--- i-Leaning/ViewController/ViewController.swift
+++ i-Leaning/ViewController/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapOnStudent(_ sender: Any) {
+      //  self.performSegue(withIdentifier: "segueStudent", sender: nil)
+
     }
     
     
